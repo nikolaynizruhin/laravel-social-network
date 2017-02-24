63a2e9f455b746444d803983990b55a7d4ad254f
Nikolay Nizruhin <lgnexus4fun@gmail.com>
2017-02-24 23:46:28 +0000

    add profile info

--- resources/views/partials/profile.blade.php
+++ resources/views/partials/profile.blade.php
@@ -1,9 +1,12 @@
 <div class="panel panel-default">
     <div class="panel-body">
+        <img src="/uploads/avatars/{{ $user->avatar }}" class="img-responsive" alt="Responsive image">
+        <br>
         <p>{{ $user->name }}</p>
-        <p>{{ $user->username }}</p>
-        <p>{{ $user->website }}</p>
-        <p>{{ $user->location }}</p>
-        <p>{{ $user->birthday }}</p>
+        <p><a href="/{{ $user->username }}">{{ '@' . $user->username }}</a></p>
+        <p><i class="fa fa-link" aria-hidden="true"></i> <a href="{{ $user->website }}">Website</a></p>
+        <p><i class="fa fa-map-marker" aria-hidden="true"></i> {{ $user->location }}</p>
+        <p><i class="fa fa-birthday-cake" aria-hidden="true"></i> {{ $user->birthday }}</p>
+        <p><i class="fa fa-calendar" aria-hidden="true"></i> {{ $user->created_at }}</p>
     </div>
 </div>
