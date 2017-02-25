155d96fd655b3eed3bf1df46507c91e91743c492
Nikolay Nizruhin <lgnexus4fun@gmail.com>
2017-02-25 10:35:50 +0200

    update date format

--- resources/views/partials/profile.blade.php
+++ resources/views/partials/profile.blade.php
@@ -6,7 +6,7 @@
         <p><a href="/{{ $user->username }}">{{ '@' . $user->username }}</a></p>
         <p><i class="fa fa-link" aria-hidden="true"></i> <a href="{{ $user->website }}">Website</a></p>
         <p><i class="fa fa-map-marker" aria-hidden="true"></i> {{ $user->location }}</p>
-        <p><i class="fa fa-birthday-cake" aria-hidden="true"></i> {{ $user->birthday }}</p>
-        <p><i class="fa fa-calendar" aria-hidden="true"></i> {{ $user->created_at }}</p>
+        <p><i class="fa fa-birthday-cake" aria-hidden="true"></i> {{ \Carbon\Carbon::parse($user->birthday)->format('j F Y') }}</p>
+        <p><i class="fa fa-calendar" aria-hidden="true"></i> {{ \Carbon\Carbon::parse($user->created_at)->format('F Y') }}</p>
     </div>
 </div>
