2e8b52fd6f263873c8b2c42fc1c2274017667702
Nikolay Nizruhin <lgnexus4fun@gmail.com>
2017-02-25 13:18:49 +0200

    add posts view

--- resources/views/partials/posts.blade.php
+++ resources/views/partials/posts.blade.php
@@ -2,8 +2,31 @@
     <div class="panel-heading">{{ $user->name }} Posts</div>
 
     <div class="panel-body">
-        @foreach($user->posts as $post)
-            <p>{{ $post->body }}</p>
+        @foreach($user->posts()->latest()->get() as $post)
+            <div class="media">
+                <div class="media-left">
+                    <a href="{{ url($user->username) }}">
+                        <img class="media-object" src="uploads/avatars/{{ $user->avatar }}" alt="avatar" style="width: 64px; height: 64px;">
+                    </a>
+                </div>
+                <div class="media-body">
+                    <h4 class="media-heading">
+                        <a href="{{ url($user->username) }}">
+                            {{ $user->name }}
+                            <small>
+                                {{ '@' . $user->username }}
+                            </small>
+                        </a>
+                        <small>
+                            &bull; {{ $post->created_at->diffForHumans() }}
+                        </small>
+                    </h4>
+                    {{ $post->body }}
+                </div>
+            </div>
+            @if($user->posts()->latest()->get()->last() != $post)
+                <hr>
+            @endif
         @endforeach
     </div>
 </div>
