da4040bfabe34a030ead0df09cb3993511736f4e
Nikolay Nizruhin <lgnexus4fun@gmail.com>
2017-02-26 16:32:05 +0200

    add policies

--- resources/views/partials/posts.blade.php
+++ resources/views/partials/posts.blade.php
@@ -1,28 +1,30 @@
 <div class="panel panel-default">
-    <div class="panel-heading">
-        <div class="media">
-            <div class="media-left">
-                <a href="{{ url($user->username) }}">
-                    <img class="media-object" src="uploads/avatars/{{ $user->avatar }}" alt="avatar" style="width: 64px; height: 64px;">
-                </a>
-            </div>
-            <div class="media-body">
-                <form method="post" action="/posts">
-                    {{ csrf_field() }}
-                    <div class="form-group {{ $errors->has('body') ? ' has-error' : '' }}">
-                        <textarea name="body" class="form-control" rows="3" placeholder="What's New?" required autofocus>{{ old('body') }}</textarea>
+    @if(Auth::user() == $user)
+        <div class="panel-heading">
+            <div class="media">
+                <div class="media-left">
+                    <a href="{{ url($user->username) }}">
+                        <img class="media-object" src="uploads/avatars/{{ $user->avatar }}" alt="avatar" style="width: 64px; height: 64px;">
+                    </a>
+                </div>
+                <div class="media-body">
+                    <form method="post" action="/posts">
+                        {{ csrf_field() }}
+                        <div class="form-group {{ $errors->has('body') ? ' has-error' : '' }}">
+                            <textarea name="body" class="form-control" rows="3" placeholder="What's New?" required autofocus>{{ old('body') }}</textarea>
 
-                        @if ($errors->has('body'))
-                            <span class="help-block">
-                                <strong>{{ $errors->first('body') }}</strong>
-                            </span>
-                        @endif
-                    </div>
-                    <button type="submit" class="pull-right btn btn-primary">Submit</button>
-                </form>
+                            @if ($errors->has('body'))
+                                <span class="help-block">
+                                    <strong>{{ $errors->first('body') }}</strong>
+                                </span>
+                            @endif
+                        </div>
+                        <button type="submit" class="pull-right btn btn-primary">Submit</button>
+                    </form>
+                </div>
             </div>
         </div>
-    </div>
+    @endif
 
     <div class="panel-body">
         @foreach($user->posts()->latest()->get() as $post)
@@ -43,16 +45,19 @@
                         <small>
                             &bull; {{ $post->created_at->diffForHumans() }}
                         </small>
-                        <a href="{{ url('posts/' . $post->id) }}"
-                           onclick="event.preventDefault();
-                                    document.getElementById('delete-post-form').submit();">
-                            <i class="pull-right fa fa-trash" aria-hidden="true"></i>
-                        </a>
 
-                        <form id="delete-post-form" action="{{ url('posts/' . $post->id) }}" method="POST" style="display: none;">
-                            {{ csrf_field() }}
-                            {{ method_field('DELETE') }}
-                        </form>
+                        @if(Auth::user() == $user)
+                            <a href="{{ url('posts/' . $post->id) }}"
+                               onclick="event.preventDefault();
+                                        document.getElementById('delete-post-form').submit();">
+                                <i class="pull-right fa fa-trash" aria-hidden="true"></i>
+                            </a>
+
+                            <form id="delete-post-form" action="{{ url('posts/' . $post->id) }}" method="POST" style="display: none;">
+                                {{ csrf_field() }}
+                                {{ method_field('DELETE') }}
+                            </form>
+                        @endif
                     </h4>
                     {{ $post->body }}
                 </div>
