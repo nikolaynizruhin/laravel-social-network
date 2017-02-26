33696a4e54186d181e4e78adad78edb9724ecaf5
Nikolay Nizruhin <lgnexus4fun@gmail.com>
2017-02-26 23:35:13 +0200

    add middleware

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,6 +8,16 @@ use Illuminate\Http\Request;
 class PostController extends Controller
 {
     /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
      * Store a new post.
      *
      * @param  Request  $request
