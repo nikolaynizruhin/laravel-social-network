6182fb0f005d604ba2ec9efad36aa73fe16a1ee8
Nikolay Nizruhin <lgnexus4fun@gmail.com>
2017-02-28 22:12:01 +0200

    fix mail view

--- resources/views/emails/post-reply.blade.php
+++ resources/views/emails/post-reply.blade.php
@@ -5,6 +5,8 @@
 </head>
 <body>
     <h1>Hi, {{ $recipient->name }}!</h1>
-    <h3>You got a reply from <a href="http://homestead.app/{{ $sender->username }} }}">{{ $sender->name }}</a>. Please take a look.</h3>
+    <h3>
+        You got a reply from <a href="http://homestead.app/{{ $sender->username }}">{{ $sender->name }}</a>.
+    </h3>
 </body>
 </html>
