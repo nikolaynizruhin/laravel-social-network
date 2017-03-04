d242afeeadaa13697c9950c90c0a6ed4724fe618
Nikolay Nizruhin <lgnexus4fun@gmail.com>
2017-03-04 10:43:06 +0200

    add avatar upload test

---
+++ tests/Feature/AvatarUploadTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\User;
+use Tests\TestCase;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class AvatarUploadTest extends TestCase
+{
+    use WithoutMiddleware;
+
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testAvatarUpload()
+    {
+        $user = factory(User::class)->create();
+
+        $response = $this->actingAs($user)->json('POST', '/profile', [
+            'avatar' => UploadedFile::fake()->image('avatar.jpg')
+        ]);
+
+        $this->assertTrue($user->avatar !== 'default.jpg');
+    }
+}
