5ad507d47e70fc01518a3516c0be85bab2435145
Nikolay Nizruhin <lgnexus4fun@gmail.com>
2017-03-04 11:03:30 +0200

    add post create test

---
+++ tests/Browser/PostCreateTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Browser;
+
+use App\User;
+use Faker\Factory;
+use Tests\DuskTestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class PostCreateTest extends DuskTestCase
+{
+    /**
+     * A Dusk test example.
+     *
+     * @return void
+     */
+    public function testUserCanCreatePost()
+    {
+        $user = factory(User::class)->create();
+
+        $faker = Factory::create();
+        $body = $faker->text($maxNbChars = 200);
+
+        $this->browse(function ($browser) use ($user, $body) {
+            $browser->loginAs($user)
+                    ->visit('/' . $user->username)
+                    ->type('body', $body)
+                    ->press('Submit')
+                    ->assertSee($body);
+        });
+    }
+}
